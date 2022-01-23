0f07aae17edddff7ceea3d6059791251df4c1c12
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-01-23 21:47:02 +0000

    Create base skeleton

--- main.cpp
+++ main.cpp
@@ -1,35 +1,60 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
-int main() {
-    glfwInit();
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+/* Class to store Vulkan objects as private class members and add functions to initiate each of them */
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
     }
 
-    glfwDestroyWindow(window);
+private:
+    /* Create pointer variable named window */
+    GLFWwindow* window;
 
-    glfwTerminate();
+    /* Function calls */
+    void initWindow() {
+        glfwInit(); // Initialise GLFW library
 
-    return 0;
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // No OpenGL context
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Disable resized windows
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+    void initVulkan() {
+
+    }
+    /* Frame rendering */
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents(); /* Keep application running until either an error occurs or window is closed */
+        }
+    }
+    /* Resource deallocation - Release resources that we've used */
+    void cleanup() {
+        glfwDestroyWindow(window); // Destroy used resource
+        glfwTerminate(); // Terminate GLFW
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl; // Describe error. Common one might be a certain required extension is not supported
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
