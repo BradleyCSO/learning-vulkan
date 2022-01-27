454176b48d4340163313660641d595c1585fcca6
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-01-27 10:39:04 +0000

    Implement validation layers via VK_LAYER_KHRONOS_validation

--- main.cpp
+++ main.cpp
@@ -1,11 +1,50 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <vector>
+#include <cstring>
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+/* Enable standard diagnostics layer provided by the Vulkan SDK */
+const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false; // Disable validation layers if not being compiled in debug mode
+#else
+    const bool enableValidationLayers = true; // Enable validation layers if being compiled in debug mode
+#endif
+
+/* Checks if all the requested layers are available */
+bool checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    /* Checks if all the layers in validationLayers exist in the availableLayers list */
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false; // Initial declared value of false
+
+        for (const auto &layerProperties: availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if (!layerFound) {
+            return false;
+        }
+    }
+        return true;
+}
 
 /* Class to store Vulkan objects as private class members and add functions to initiate each of them */
 class HelloTriangleApplication {
@@ -50,6 +89,10 @@ private:
     }
 
     void createInstance() {
+        if (enableValidationLayers && !checkValidationLayerSupport()) {
+            throw std::runtime_error("validation layers requested, but not available!");
+        }
+
         /* Optional - Create VkApplicationInfo struct to fill information about the application */
         VkApplicationInfo appInfo{}; // Create appInfo struct
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -66,12 +109,19 @@ private:
 
         /* GLFW - Interface with window system via a function that returns the extension(s) needed by passing to the struct */
         uint32_t glfwExtensionCount = 0;
-        const char** glfwExtensions;
+        const char **glfwExtensions;
         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
         createInfo.enabledExtensionCount = glfwExtensionCount;
         createInfo.ppEnabledExtensionNames = glfwExtensions;
-        createInfo.enabledLayerCount = 0;
+
+        /* Include validation layer names if they are enabled */
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        } else {
+            createInfo.enabledLayerCount = 0;
+        }
 
         // Now have everything Vulkan needs to create an instance, so issue the vkCreateInstance call (no need to store the result)
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
